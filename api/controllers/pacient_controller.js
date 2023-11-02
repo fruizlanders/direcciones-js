b816bc69fbea9953bb06e678b773ae27c85b2f92
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-11-01 20:41:39 -0500

    se obtuvo la imagen del servidor pypde

--- api/controllers/pacient_controller.js
+++ api/controllers/pacient_controller.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const unirest = require('unirest');
+const axios = require('axios');
 var express = require('express');
 var router = express.Router();
 var helpers = require('../../config/helpers');
@@ -11,20 +11,26 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/generate-image', async (req, res, next) => {
+  
   const exp = req.query.exp;
   try {
     var url = 'http://192.168.1.27:8080/image?exp=' + exp;
-    const response = await unirest.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        'Accept': 'image/png', // Establece la cabecera "Accept" como "image/png"
+      },
+      responseType: 'arraybuffer', // Configura el tipo de respuesta como array de bytes (para imágenes)
+    }); // Reemplaza la URL con la que desees hacer la solicitud
     if (response.status === 200) {
-      var imageData = response.body;
+      var imageData = response.data;
       var randName = helpers.randomSN(20);
       var savePath = '../../public/pypde-images/' + randName + '.png'
       try {
-        console.log(path.resolve(__dirname, savePath))
-        await fs.writeFile(path.resolve(__dirname, savePath), imageData, (err) => {
+        // console.log(path.resolve(__dirname, savePath))
+        fs.writeFile(path.resolve(__dirname, savePath), imageData, 'binary', (err) => {
           if (err) {
-            console.error(err);
-            res.send('Error al guardar la imagen en el disco').status(500);
+            console.error('Error al guardar la imagen en el disco:', err);
+            res.send('Error en escribir el archivo').status(500);
           } else {
             res.send('pypde-images/' + randName + '.png').status(200);
           }
